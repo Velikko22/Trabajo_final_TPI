2b2f686cce9a8afa23ab61ed1d46270db2ec269a
Julian Capellino <juliancapellino@gmail.com>
2024-05-26 20:31:49 -0300

    creacion comando github - actualizacion 26/5/24

---
+++ Clases/Tratamiento.py
@@ -0,0 +1,12 @@
+class Tratamientos:
+    def __init__(self, nombreTratamiento, duracionTratamiento):
+        self.nombreTratamiento = nombreTratamiento
+        self.duracionTratamiento = duracionTratamiento
+    def addTratamiento(self):
+        pass
+    def addDuracion(self):
+        pass
+    def __str__(self):
+        return "STR"
+    def __repr__(self):
+        return "REPR"
