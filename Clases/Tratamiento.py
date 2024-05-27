da7012c8b75595078c798bcc2ae5f54eec96a880
TotiManuel <manuel.mandaio@gmail.com>
2024-05-27 14:12:33 -0300

    Clase Tratamiento pasa a prueba - 27/05/24 - 14:12hs

--- Clases/Tratamiento.py
+++ Clases/Tratamiento.py
@@ -2,14 +2,23 @@ class Tratamientos:
     def __init__(self, nombreTratamiento, duracionTratamiento):
         self.nombreTratamiento = nombreTratamiento
         self.duracionTratamiento = duracionTratamiento
-    def addTratamiento(self):
-        pass
-    def addDuracion(self):
-        pass
+    def addTratamiento(self, newNombre, newDuracion):
+        with open("Datos archivos.txt\Tratamiento.txt", 'a') as archivo:
+            nueva_linea = newNombre + "," + newDuracion + "\n"
+            archivo.write(nueva_linea)
+    def getNombreTratamiento(self):
+        return self.nombreTratamiento
+    def getDuracionTratamiento(self):
+        return self.duracionTratamiento
+    def setNombreTratamiento(self, newNombre):
+        self.nombreTratamiento = newNombre
+    def setDuracionTratamiento(self, newDuracion):
+        self.duracionTratamiento = newDuracion
     def __str__(self):
         return "STR"
     def __repr__(self):
         return "REPR"
 
 if __name__=="__main__":
-    pass
+    prueba = Tratamientos("Caca", "3 Dias")
+    prueba.addTratamiento("caca", "3 Dias")
