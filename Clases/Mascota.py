a0ab84d9beeecf2df235349af1796fb6bcb87619
Paulozzt <paulozt5@gmail.com>
2024-06-05 00:12:53 -0300

    Clase Raza y Main modificados 30/5

---
+++ Clases/Mascota.py
@@ -0,0 +1,58 @@
+class Mascota():
+    def __init__(self, tipoAnimalRaza, nombreRazaAnimal,identificador,propietario, nombreAnimal,  historial, stateMascota):
+        self.tipoAnimalRaza = tipoAnimalRaza
+        self.nombreRazaAnimal = nombreRazaAnimal
+        self.identificador = identificador
+        self.propietario = propietario
+        self.nombreAnimal = nombreAnimal
+        self.historial = historial
+        self.stateMascota = stateMascota
+
+
+
+    def get_tipoAnimalRaza(self):
+        return self.tipoAnimalRaza
+
+    def get_nombreRazaAnimal(self):
+        return self.nombreRazaAnimal
+
+    def get_identificador(self):
+        return self.identificador
+
+
+    def get_propietario(self):
+        return self.propietario
+
+    def get_nombreAnimal(self):
+        return self.nombreAnimal
+
+    def get_historial(self):
+        return self.historial
+
+    def get_stateMascota(self):
+        return self.stateMascota
+
+    def set_tipoAnimalRaza(self, tipoAnimalRaza):
+        self.tipoAnimalRaza = tipoAnimalRaza
+
+    def set_nombreRazaAnimal(self, nombreRazaAnimal):
+        self.nombreRazaAnimal = nombreRazaAnimal
+
+    def set_identificador(self, identificador):
+        self.identificador = identificador
+
+    def set_propietario(self, propietario):
+        self.propietario = propietario
+
+    def set_nombreAnimal(self, nombreAnimal):
+        self.nombreAnimal = nombreAnimal
+
+    def set_historial(self, historial):
+        self.historial = historial
+
+    def set_stateMascota(self, stateMascota):
+        self.stateMascota = stateMascota
+
+    def __repr__(self):
+        return (f"{self.tipoAnimalRaza}, {self.nombreRazaAnimal}, {self.identificador}, {self.propietario}, "
+                f"{self.nombreAnimal}, {self.historial},{self.stateMascota}")
