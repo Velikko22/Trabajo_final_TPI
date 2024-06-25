6ce6960970f8711f6445124a4afed8eb88abbec7
julianbrochero <143361943+julianbrochero@users.noreply.github.com>
2024-06-25 02:57:16 -0300

    Add files via upload
    Modificacion para funcionamiento de Tratamiento.

--- MVC/Modelo/Tratamiento.py
+++ MVC/Modelo/Tratamiento.py
@@ -1,29 +1,33 @@
 class Tratamiento:
-    def __init__(self, nombreTratamiento, duracionTratamiento,state):
+    def __init__(self, nombreTratamiento, duracionTratamiento, state):
         self.nombreTratamiento = nombreTratamiento
         self.duracionTratamiento = duracionTratamiento
         self.state = state
 
+    def tratamientoActivo(self):
+        if self.state == 1:
+            return True
+
     def getNombreTratamiento(self):
         return self.nombreTratamiento
-    
+
     def getDuracionTratamiento(self):
         return self.duracionTratamiento
-    
+
     def getState(self):
         return self.state
-    
-    def setNombretratamiento(self,nombretratamiento):
+
+    def setNombretratamiento(self, nombretratamiento):
         self.nombreTratamiento = nombretratamiento
-        
-    def setDuracionTratamiento(self,duracionTratamiento):
+
+    def setDuracionTratamiento(self, duracionTratamiento):
         self.duracionTratamiento = duracionTratamiento
-    
-    def setState(self,state):
+
+    def setState(self, state):
         self.state = state
-    
+
     def __str__(self):
-        return "STR"
-    
+        return (f"{self.nombreTratamiento}, {self.duracionTratamiento}, {self.state}")
+
     def __repr__(self):
-        return "REPR"
+        return (f"{self.nombreTratamiento}, {self.duracionTratamiento}, {self.state}")
